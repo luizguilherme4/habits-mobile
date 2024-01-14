e44ab354989548c23259b1ea2264ebab94354a72
Luiz Guilherme <101076506+luizguilherme4@users.noreply.github.com>
2024-01-13 22:36:00 -0400

    feat: add minimum summary dates

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 
 import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 
@@ -7,6 +7,8 @@ import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
 const datesFromYearsStart = generateDatesFromYearBeginning();
+const minimumSummaryDatesSizes = 18 * 7;
+const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearsStart.length;
 
 export function Home() {
   return (
@@ -27,15 +29,29 @@ export function Home() {
         }
       </View>
 
-      <View className="flex-row flex-wrap">
-        {
-          datesFromYearsStart.map(date => (
-            <HabitDay
-              key={date.toISOString()}
-            />
-          ))
-        }
-      </View>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="flex-row flex-wrap">
+          {
+            datesFromYearsStart.map(date => (
+              <HabitDay
+                key={date.toISOString()}
+              />
+            ))
+          }
+
+          {
+            amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => (
+              <View
+                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              />
+            ))
+          }
+        </View>
+      </ScrollView>
     </View>
   )
 }
