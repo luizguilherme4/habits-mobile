2f5121f8f1d2771c21f844635c05938ceef26c87
Luiz Guilherme <101076506+luizguilherme4@users.noreply.github.com>
2024-01-29 21:56:56 -0400

    feat: get habit summary

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,7 @@
-import { View, Text, ScrollView } from "react-native";
+import { useEffect, useState } from "react";
+import { View, Text, ScrollView, Alert } from "react-native";
 
+import { api } from "../lib/axios";
 import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 
 import { Header } from "../components/Header";
@@ -12,7 +14,29 @@ const minimumSummaryDatesSizes = 18 * 7;
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearsStart.length;
 
 export function Home() {
+  const [loading, setLoading] = useState(true);
+  const [summary, setSummary] = useState(null);
+
   const { navigate } = useNavigation();
+
+  async function fetchData() {
+    try {
+      console.log(api.getUri());
+      setLoading(true);
+      const response = await api.get('/summary');
+      console.log(response.data);
+      setSummary(response.data);
+    } catch (error) {
+      Alert.alert('Ops', 'Não foi possível carregar o sumário de hábitos');
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
 
   return (
     <View className="flex-1 bg-background px-8 pt-16">
