2268a958c41acb1d65f3e7c39baa7d53a736f2ca
Luiz Guilherme <101076506+luizguilherme4@users.noreply.github.com>
2024-02-21 21:26:24 -0400

    feat: list day habits

--- src/screens/Habit.tsx
+++ src/screens/Habit.tsx
@@ -1,22 +1,64 @@
+import { useEffect, useState } from "react";
 import { useRoute } from "@react-navigation/native";
-import { ScrollView, Text, View } from "react-native";
+import { Alert, ScrollView, Text, View } from "react-native";
+
 import dayjs from "dayjs";
 
+import { api } from "../lib/axios";
 import { BackButton } from "../components/BackButton";
 import { ProgressBar } from "../components/ProgressBar";
 import { Checkbox } from "../components/Checkbox";
+import { Loading } from "../components/Loading";
 
 interface Params {
   date: string;
 }
 
+interface DayInfoProps {
+  completedHabits: string[];
+  possibleHabits: {
+    id: string;
+    title: string;
+  }[]
+}
+
 export function Habit() {
+  const [loading, setLoading] = useState(false);
+  const [dayInfo, setDayInfo] = useState<DayInfoProps | null>(null);
+  const [completedHabits, setCompletedHabits] = useState<string[]>([]);
+
   const route = useRoute();
   const { date } = route.params as Params;
 
   const parsedDate = dayjs(date);
   const dayOfWeek = parsedDate.format("dddd");
   const dayAndMonth = parsedDate.format("DD/MM");
+
+  async function fetchHabits() {
+    try {
+      setLoading(true)
+
+      const response = await api.get("/day", { params: { date } })
+      setDayInfo(response.data);
+      setCompletedHabits(response.data.completedHabits);
+
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Ops", "Não foi possível carregar as informações dos hábitos")
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchHabits()
+  }, [])
+
+  if (loading) {
+    return (
+      <Loading />
+    )
+  }
 
   return (
     <View className="flex-1 bg-background px-8 pt-16">
@@ -37,15 +79,16 @@ export function Habit() {
         <ProgressBar progress={70} />
 
         <View className="mt-6">
-          <Checkbox
-            title="Beber 2l de água"
-            checked={false}
-          />
-
-          <Checkbox
-            title="Caminhar"
-            checked={true}
-          />
+          {
+            dayInfo?.possibleHabits &&
+            dayInfo.possibleHabits.map(habit => (
+              <Checkbox
+                key={habit.id}
+                title={habit.title}
+                checked={completedHabits.includes(habit.id)}
+              />
+            ))
+          }
         </View>
       </ScrollView>
     </View>
